3ace14d4d0b272b73a63f3103c804be0be5bdbbc
Richard Taujenis <Mozesthegreat@yahoo.com>
2022-08-29 23:24:53 +0200

    Added .env to store API, pages/PopularPage displays recepies

--- client/recepie-app/src/pages/PopularPage.tsx
+++ client/recepie-app/src/pages/PopularPage.tsx
@@ -1,3 +1,52 @@
+import { useEffect, useState } from "react";
+// const dotenv = require('dotenv');
+
 export function Popular() {
-  return <h1>Popular Page</h1>;
+  const [popular, setPopular] = useState([] as any[]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+const getPopular = async () => {
+  const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_URL}&number=10`)
+  const data = await api.json();
+  console.log(data.recipes)
+  
+  setPopular(data.recipes);
+};
+  return (
+    <div>
+      <div className="box cta">
+      <p className="has-text-centered">
+        <span className="tag is-primary">TOP</span> 10 Popular
+      </p>
+    </div>
+    <section className="container">
+        <div className="columns features">
+          {popular.map((recepie, i) => {
+            return(
+              <div className="column is-4">
+                <div className="card is-shady">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={recepie.image} alt={recepie.title} className="modal-button" data-target="modal-image2" />
+                    </figure>
+                  </div>
+                <div className="card-content">
+                  <div className="content">
+                    <h4>{recepie.title}</h4>
+                    {/* <p>{recepie.summary}</p> */}
+                    <span className="button is-link modal-button" data-target="modal-image2">Image modal</span>
+                  </div>
+                </div>
+            </div>
+        </div>
+            )
+          }
+          )}
+        </div>
+    </section>
+    </div>
+  )
 }
