8bcae02ba0de56519a80c4b149313d305c1db0d9
Richard Taujenis <Mozesthegreat@yahoo.com>
2022-09-27 23:49:50 +0200

    Added home screen with input search for recepie

--- client/recepie-app/src/components/Searched.tsx
+++ client/recepie-app/src/components/Searched.tsx
@@ -4,7 +4,16 @@ import "@splidejs/splide/dist/css/splide.min.css";
 
 const Searched: React.FC = () => {
     return (
-        <h5>Search</h5>
+        <div className="has-addons-fullwidth">
+            <div className="control">
+                <input className="input" type="text" placeholder="Find a recipe" />
+            </div>
+            <div className="control">
+                <a className="button is-info">
+                    Search
+                </a>
+            </div>
+        </div>
     )
 }
 export default Searched;
--- client/recepie-app/src/pages/HomePage.tsx
+++ client/recepie-app/src/pages/HomePage.tsx
@@ -3,7 +3,18 @@ import Searched from '../components/Searched'
 
 const HomePage: React.FC = () => {
   return (
-      <Searched />
+      <section className="section py-4">
+        <div className="container">
+          <div className="column is-multiline is-centered">
+            <div className="column has-text-centered">
+              <span className="has-text-gray-dark">Welcome to My Recepie's</span>
+              <h2 className="mt-2 mb-4 is-size-1 is-size-3-mobile has-text-weight-bold">Find recepies of your liking in outer pages and add them to your cooking wish list.</h2>
+              <p className="subtitle has-text-gray mb-5">Otherwise search for anything particular you would like to cook bellow!</p>
+              <Searched />
+            </div>
+          </div>
+        </div>
+      </section>
   )
 }
 export default HomePage;
