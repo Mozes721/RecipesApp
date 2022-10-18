e16230375760e5c399733f47e73471bacb284956
Richard Taujenis <Mozesthegreat@yahoo.com>
2022-10-18 22:47:48 +0200

    condition is being rendered if string or number api returned as Object

--- client/recepie-app/src/components/searched/SearchedResult.tsx
+++ client/recepie-app/src/components/searched/SearchedResult.tsx
@@ -8,81 +8,76 @@ interface Props {
 }
 
 const SearchedReturn: React.FC<Props> = (props)=> {
-    const [searched, setSearched] = useState([] as any[]);
-    useEffect(() => {
-        getMeal();
-    },[]);
+    const [searched, setSearched] = useState("");
 
-    const getMeal = async () => {
-        if (typeof props.text == "string") {
-            const api = await fetch(`https://api.spoonacular.com/mealplanner/generate?apiKey=${process.env.REACT_APP_API_URL}&timeFrame=day&type=${props.text}&number=1`)
-            const data = await api.json();
-            setSearched(data.recepies);
+    console.log(searched);
+
+    useEffect(() => {
+
+        if (isNaN(props.text as number)) {
+            getMealAsType();
         } else {
-            const api = await fetch(`https://api.spoonacular.com/mealplanner/generate?apiKey=${process.env.REACT_APP_API_URL}&timeFrame=day&targetCalories=${props.text}&number=1`)
-            const data = await api.json();
-            setSearched(data.recepies);
+            getMealAsCaloire();
         }
 
+    }, [props.text]);
+
+    const getMealAsType = async () => {
+        console.log(props.text);
+        const api = await fetch(`https://api.spoonacular.com/mealplanner/generate?apiKey=${process.env.REACT_APP_API_URL}&timeFrame=day&type=${props.text}&number=1`)
+        console.log(api)
+        const data = await api.json();
+        setSearched(data);
     }
+    const getMealAsCaloire = async () => {
+        const api = await fetch(`https://api.spoonacular.com/mealplanner/generate?apiKey=${process.env.REACT_APP_API_URL}&timeFrame=day&targetCalories=${props.text}&number=1`)
+        console.log(api)
+        const data = await api.json();
+        setSearched(data);
 
-    return (
-        <div className="container">
-            {searched.map(recepie) => {
-                return (
-                    <div>
-            <section className="nutrients">
-                <div className="level-item has-text-centered">
-                    <div>
-                        <p className="heading">Calories</p>
-                        <p className="title">{searched.nutrients.calories.toFixed(0)}</p>
-                    </div>
-                </div>
-                <div className="level-item has-text-centered">
-                    <div>
-                        <p className="heading">Following</p>
-                        <p className="title">123</p>
-                    </div>
-                </div>
-                <div className="level-item has-text-centered">
-                    <div>
-                        <p className="heading">Followers</p>
-                        <p className="title">456K</p>
-                    </div>
-                </div>
-                <div className="level-item has-text-centered">
-                    <div>
-                        <p className="heading">Likes</p>
-                        <p className="title">789</p>
-                    </div>
-                </div>
-            </section>
-
-            <section>
-            <div className="card is-shady column is-10">
-                <div className="card-image">
-                    <figure className="image is-3by2">
-                        <img src={recepie.image} alt={recepie.title} className="modal-button" data-target="modal-image2" />
-                    </figure>
-                </div>
-                <div className="card-content ">
-                    <div className="content">
-                        <h5>{recepie.title}</h5>
-                        <div className="columns is-mobile  is-centered is-one-quarter">
-                            <a href={recepie.sourceUrl} target="_blank" rel="noreferrer">
-                                <span className="button is-info modal-button column is-narrow" data-target="modal-image2">Link</span>
-                            </a>
-                            <span className="button is-success column is-narrow" data-target="modal-image2">Add to Yumms</span>
+    }
+        if(searched !== "") {
+            return (
+                <main>
+                <section className="nutritions">
+                    <nav className="level">
+                        <div className="level-item has-text-centered">
+                            <div>
+                                <p className="heading">Calories</p>
+                                <p className="title"></p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-                </div>
-            })}
-        </div>
-
-    )
+                        <div className="level-item has-text-centered">
+                            <div>
+                                <p className="heading">Protein</p>
+                                <p className="title">123</p>
+                            </div>
+                        </div>
+                        <div className="level-item has-text-centered">
+                            <div>
+                                <p className="heading">Fat</p>
+                                <p className="title">456K</p>
+                            </div>
+                        </div>
+                        <div className="level-item has-text-centered">
+                            <div>
+                                <p className="heading">Carbohydrates</p>
+                                <p className="title">789</p>
+                            </div>
+                        </div>
+                    </nav>
+                </section>
+                {/*<section className="recepie">*/}
+                {/*    /!*{searched.map((recepie) => {*!/*/}
+                {/*    /!*    return (*!/*/}
+                {/*    /!*        <p>recepie</p>*!/*/}
+                {/*    /!*    )*!/*/}
+                {/*    /!*})}*!/*/}
+                {/*</section>*/}
+                </main>
+            )
+        }
+        return null;
 }
 
 
