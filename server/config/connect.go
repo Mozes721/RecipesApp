c2c03344ecd597de5b6eb94a56e5db8b989b1a7b
Mozes721 <MozesThegreat@yahoo.com>
2024-09-27 12:43:00 +0200

    adjusted for deployment

--- server/config/connect.go
+++ server/config/connect.go
@@ -6,18 +6,16 @@ import (
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
 	"fmt"
+	"github.com/gobuffalo/envy"
 	"github.com/redis/go-redis/v9"
 	"google.golang.org/api/option"
 	"os"
 )
 
 func firebaseApp(ctx context.Context) (*firebase.App, error) {
-	//account_key, err := envy.MustGet("FIREBASE_ACCOUNT_KEY_LOCATION")
-	//if err != nil {
-	//	return nil, err
-	//}
+	account_key := envy.Get("FIREBASE_ACCOUNT_KEY_LOCATION", "/mnt/c/own_dev/RecipesApp/server/config/account_key.json")
 
-	opt := option.WithCredentialsFile("/mnt/c/own_dev/RecipesApp/server/config/account_key.json")
+	opt := option.WithCredentialsFile(account_key)
 	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
 		return nil, err
